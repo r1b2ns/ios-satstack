852cf0089119ea77bce03d6e4be0c7a63322ea38
Rubens <r1b2ns@duck.com>
2026-02-28 12:13:40 -0300

    feat: refactor viewModels to use uiState

--- MempoolMonitor/MempoolMonitor/Modules/TransactionList/TransactionListView/TransactionListViewModel.swift
+++ MempoolMonitor/MempoolMonitor/Modules/TransactionList/TransactionListView/TransactionListViewModel.swift
@@ -2,9 +2,17 @@ import Foundation
 import SwiftUI
 
 protocol TransactionListViewModelProtocol: ObservableObject {
+    var uiState: TransactionListUiState { get set }
+}
+
+struct TransactionListUiState {
     
 }
 
 final class TransactionListViewModel: TransactionListViewModelProtocol {
+    @Published var uiState: TransactionListUiState
     
+    init(uiState: TransactionListUiState = .init()) {
+        self.uiState = uiState
+    }
 }
